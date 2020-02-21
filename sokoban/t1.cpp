4dc96ff2deaa79f5a4c3c9f27918b089820efa93
BruhP8 <luc.biohazard@gmail.com>
2020-02-21 14:16:45 +0100

    tests

--- sokoban/t1.cpp
+++ sokoban/t1.cpp
@@ -7,22 +7,40 @@
 
 int main(int _ac, char** _av) {
   int nb_test_ok = 0;
-  if(test_p001()==false) printf("test_p001 FAILED\n");
+  if(test_u001()==false) printf("test_u001 FAILED\n");
   else nb_test_ok++;
 
-  if(test_p002()==false) printf("test_p002 FAILED\n");
+  if(test_u002()==false) printf("test_u002 FAILED\n");
   else nb_test_ok++;
 
-  if(test_p003()==false) printf("test_p003 FAILED\n");
+  if(test_u003()==false) printf("test_u003 FAILED\n");
   else nb_test_ok++;
 
-  if(test_p004()==false) printf("test_p004 FAILED\n");
+  if(test_u004()==false) printf("test_u004 FAILED\n");
   else nb_test_ok++;
 
-  if(test_p005()==false) printf("test_p005 FAILED\n");
+  if(test_u005()==false) printf("test_u005 FAILED\n");
   else nb_test_ok++;
 
-  if(test_p006()==false) printf("test_p005 FAILED\n");
+  if(test_u006()==false) printf("test_u006 FAILED\n");
+  else nb_test_ok++;
+  
+  if(test_u007()==false) printf("test_u007 FAILED\n");
+  else nb_test_ok++;
+  
+  if(test_u008()==false) printf("test_u008 FAILED\n");
+  else nb_test_ok++;
+
+  if(test_u009()==false) printf("test_u009 FAILED\n");
+  else nb_test_ok++;
+
+  if(test_u010()==false) printf("test_u010 FAILED\n");
+  else nb_test_ok++;
+
+  if(test_u011()==false) printf("test_u011 FAILED\n");
+  else nb_test_ok++;
+
+  if(test_u012()==false) printf("test_u012 FAILED\n");
   else nb_test_ok++;
   //...
   printf("nb_test_ok: %d\n", nb_test_ok);
--- sokoban/testsok.h
+++ sokoban/testsok.h
@@ -5,7 +5,7 @@
 /* position : ...#
  *            ...@
  * can_move_U() return false */
-bool test_p001() {
+bool test_u001() {
   board[0][0]=WALL; board[0][1]=END_OF_LINE;
   board[1][0]=MAN_ON_FREE;board[1][1]=END_OF_LINE;
   board_nbl=2;
@@ -16,7 +16,7 @@ bool test_p001() {
 /* position : ...#
  *            ...a
  * can_move_U() return false */
-bool test_p002() {
+bool test_u002() {
   board[0][0]=WALL; board[0][1]=END_OF_LINE;
   board[1][0]=MAN_ON_TARGET;board[1][1]=END_OF_LINE;
   board_nbl=2;
@@ -32,7 +32,7 @@ bool test_p002() {
  *                                ...@
  *                                ...
 */
-bool test_p003() {
+bool test_u003() {
   board[0][0]=WALL; board[0][1]=END_OF_LINE;
   board[1][0]=FREE; board[1][1]=END_OF_LINE;
   board[2][0]=MAN_ON_FREE;board[2][1]=END_OF_LINE;
@@ -54,7 +54,7 @@ bool test_p003() {
  *                                ...a
  *                                ...
 */
-bool test_p004() {
+bool test_u004() {
   board[0][0]=WALL; board[0][1]=END_OF_LINE;
   board[1][0]=FREE; board[1][1]=END_OF_LINE;
   board[2][0]=MAN_ON_TARGET;board[2][1]=END_OF_LINE;
@@ -75,7 +75,7 @@ bool test_p004() {
  *                                ...@
  *                                ...
 */
-bool test_p005() {
+bool test_u005() {
   board[0][0]=FREE; board[0][1]=END_OF_LINE;
   board[1][0]=CRATE_ON_FREE; board[1][1]=END_OF_LINE;
   board[2][0]=MAN_ON_FREE;board[2][1]=END_OF_LINE;
@@ -98,7 +98,52 @@ bool test_p005() {
  *                                ...a
  *                                ...
 */
-bool test_p006() {
+bool test_u006() {
+  board[0][0]=FREE; board[0][1]=END_OF_LINE;
+  board[1][0]=CRATE_ON_FREE; board[1][1]=END_OF_LINE;
+  board[2][0]=MAN_ON_TARGET;board[2][1]=END_OF_LINE;
+  board_nbl=3;
+  man_l=2; man_c=0;
+  if(can_move_U()==false) return false;
+  move_U();
+  if(board[0][0] != CRATE_ON_FREE) return false;
+  if(board[1][0] != MAN_ON_FREE) return false;
+  if(board[2][0] != TARGET) return false;
+  if(man_l!=1) return false;
+  return true;
+}
+
+/* position : ...
+ *            ...*
+ *            ...@
+ * can_move_U() return true 
+ * move_U() modifie la position : ...
+ *                                ...a
+ *                                ...
+*/
+bool test_u007() {
+  board[0][0]=FREE; board[0][1]=END_OF_LINE;
+  board[1][0]=TARGET; board[1][1]=END_OF_LINE;
+  board[2][0]=MAN_ON_FREE;board[2][1]=END_OF_LINE;
+  board_nbl=3;
+  man_l=2; man_c=0;
+  if(can_move_U()==false) return false;
+  move_U();
+  if(board[1][0] != MAN_ON_TARGET) return false;
+  if(board[2][0] != FREE) return false;
+  if(man_l!=1) return false;
+  return true;
+}
+
+/* position : ...
+ *            ...*
+ *            ...a
+ * can_move_U() return true 
+ * move_U() modifie la position : ...
+ *                                ...a
+ *                                ...*
+*/
+bool test_u008() {
   board[0][0]=FREE; board[0][1]=END_OF_LINE;
   board[1][0]=TARGET; board[1][1]=END_OF_LINE;
   board[2][0]=MAN_ON_TARGET;board[2][1]=END_OF_LINE;
@@ -106,10 +151,316 @@ bool test_p006() {
   man_l=2; man_c=0;
   if(can_move_U()==false) return false;
   move_U();
-  if(board[1][0]!=MAN_ON_TARGET) return false;
-  if(board[2][0]!=FREE) return false;
+  if(board[1][0] != MAN_ON_TARGET) return false;
+  if(board[2][0] != TARGET) return false;
   if(man_l!=1) return false;
   return true;
 }
 
+/* position : ...#
+ *            ...$
+ *            ...@
+ * can_move_U() return false 
+*/
+bool test_u009() {
+  board[0][0]=WALL; board[0][1]=END_OF_LINE;
+  board[1][0]=CRATE_ON_FREE; board[1][1]=END_OF_LINE;
+  board[2][0]=MAN_ON_FREE;board[2][1]=END_OF_LINE;
+  board_nbl=3;
+  man_l=2; man_c=0;
+  if(can_move_U()==false) return false;
+}
+
+/* position : ...#
+ *            ...$
+ *            ...a
+ * can_move_U() return false                              ...
+*/
+bool test_u010() {
+  board[0][0]=WALL; board[0][1]=END_OF_LINE;
+  board[1][0]=CRATE_ON_FREE; board[1][1]=END_OF_LINE;
+  board[2][0]=MAN_ON_TARGET;board[2][1]=END_OF_LINE;
+  board_nbl=3;
+  man_l=2; man_c=0;
+  if(can_move_U()==false) return false;
+}
+
+/* position : ...
+ *            ...*
+ *            ...@
+ * can_move_U() return true 
+ * move_U() modifie la position : ...
+ *                                ...a
+ *                                ...
+*/
+bool test_u011() {
+  board[0][0]=WALL; board[0][1]=END_OF_LINE;
+  board[1][0]=TARGET; board[1][1]=END_OF_LINE;
+  board[2][0]=MAN_ON_FREE;board[2][1]=END_OF_LINE;
+  board_nbl=3;
+  man_l=2; man_c=0;
+  if(can_move_U()==false) return false;
+  move_U();
+  if(board[0][0] != WALL) return false;
+  if(board[1][0] != MAN_ON_TARGET) return false;
+  if(board[2][0] != FREE) return false;
+  if(man_l!=1) return false;
+  return true;
+}
+
+/* position : ...
+ *            ...*
+ *            ...@
+ * can_move_U() return true 
+ * move_U() modifie la position : ...
+ *                                ...a
+ *                                ...
+*/
+bool test_u012 () {
+  board[0][0]=WALL; board[0][1]=END_OF_LINE;
+  board[1][0]=TARGET; board[1][1]=END_OF_LINE;
+  board[2][0]=MAN_ON_TARGET;board[2][1]=END_OF_LINE;
+  board_nbl=3;
+  man_l=2; man_c=0;
+  if(can_move_U()==false) return false;
+  move_U();
+  if(board[0][0] != WALL) return false;
+  if(board[1][0] != MAN_ON_TARGET) return false;
+  if(board[2][0] != TARGET) return false;
+  if(man_l!=1) return false;
+  return true;
+}
+
+/* -------------- DOWN ------------- */
+
+/* position : ...@
+ *            ...#
+ * can_move_D() return false */
+bool test_d001() {
+  board[0][0]=MAN_ON_FREE; board[0][1]=END_OF_LINE;
+  board[1][0]=WALL;board[1][1]=END_OF_LINE;
+  board_nbl=2;
+  man_l=0; man_c=0;
+  return (can_move_D()==false);
+}
+
+/* position : ...a
+ *            ...#
+ * can_move_U() return false */
+bool test_d002() {
+  board[0][0]=MAN_ON_TARGET; board[0][1]=END_OF_LINE;
+  board[1][0]=WALL;board[1][1]=END_OF_LINE;
+  board_nbl=2;
+  man_l=0; man_c=0;
+  return (can_move_D()==false);
+}
+
+/* position : ...@
+ *            ...
+ *            ...#
+ * can_move_D() return true 
+ * move_D() modifie la position : ...
+ *                                ...@
+ *                                ...#
+*/
+bool test_d003() {
+  board[0][0]=MAN_ON_FREE; board[0][1]=END_OF_LINE;
+  board[1][0]=FREE; board[1][1]=END_OF_LINE;
+  board[2][0]=WALL;board[2][1]=END_OF_LINE;
+  board_nbl=3;
+  man_l=0; man_c=0;
+  if(can_move_D()==false) return false;
+  move_D();
+  if(board[1][0]!=MAN_ON_FREE) return false;
+  if(board[0][0]!=FREE) return false;
+  if(man_l!=1) return false;
+  return true;
+}
+
+/* position : ...a
+ *            ...
+ *            ...#
+ * can_move_D() return true 
+ * move_D() modifie la position : ...*
+ *                                ...@
+ *                                ...#
+*/
+bool test_d004() {
+  board[0][0]=MAN_ON_TARGET; board[0][1]=END_OF_LINE;
+  board[1][0]=FREE; board[1][1]=END_OF_LINE;
+  board[2][0]=WALL;board[2][1]=END_OF_LINE;
+  board_nbl=3;
+  man_l=0; man_c=0;
+  if(can_move_D()==false) return false;
+  move_D();
+  if(board[0][0] != TARGET) return false;
+  if(board[1][0] != MAN_ON_FREE) return false;
+  if(man_l!=1) return false;
+  return true;
+}
+
+/* position : ...@
+ *            ...$
+ *            ...
+ * can_move_D() return true 
+ * move_D() modifie la position : ...
+ *                                ...@
+ *                                ...$
+*/
+bool test_d005() {
+  board[0][0]=MAN_ON_FREE; board[0][1]=END_OF_LINE;
+  board[1][0]=CRATE_ON_FREE; board[1][1]=END_OF_LINE;
+  board[2][0]=FREE;board[2][1]=END_OF_LINE;
+  board_nbl=3;
+  man_l=0; man_c=0;
+  if(can_move_D()==false) return false;
+  move_D();
+  if(board[0][0] != FREE) return false;
+  if(board[1][0] != MAN_ON_FREE) return false;
+  if(board[2][0] != CRATE_ON_FREE) return false;
+  if(man_l!=1) return false;
+  return true;
+}
+
+/* position : ...a
+ *            ...$
+ *            ...
+ * can_move_D() return true 
+ * move_D() modifie la position : ...*
+ *                                ...@
+ *                                ...$
+*/
+bool test_d006() {
+  board[0][0]=MAN_ON_TARGET; board[0][1]=END_OF_LINE;
+  board[1][0]=CRATE_ON_FREE; board[1][1]=END_OF_LINE;
+  board[2][0]=FREE;board[2][1]=END_OF_LINE;
+  board_nbl=3;
+  man_l=0; man_c=0;
+  if(can_move_D()==false) return false;
+  move_D();
+  if(board[0][0] != TARGET) return false;
+  if(board[1][0] != MAN_ON_FREE) return false;
+  if(board[2][0] != CRATE_ON_FREE) return false;
+  if(man_l!=1) return false;
+  return true;
+}
+
+/* position : ...@
+ *            ...*
+ *            ...
+ * can_move_D() return true 
+ * move_D() modifie la position : ...
+ *                                ...a
+ *                                ...
+*/
+bool test_d007() {
+  board[0][0]=MAN_ON_FREE; board[0][1]=END_OF_LINE;
+  board[1][0]=TARGET; board[1][1]=END_OF_LINE;
+  board[2][0]=FREE;board[2][1]=END_OF_LINE;
+  board_nbl=3;
+  man_l=0; man_c=0;
+  if(can_move_D()==false) return false;
+  move_D();
+  if(board[0][0] != FREE) return false;
+  if(board[1][0] != MAN_ON_TARGET) return false;
+  if(board[2][0] != FREE) return false;
+  if(man_l!=1) return false;
+  return true;
+}
+
+/* position : ...a
+ *            ...*
+ *            ...
+ * can_move_U() return true 
+ * move_U() modifie la position : ...*
+ *                                ...a
+ *                                ...
+*/
+bool test_d008() {
+  board[0][0]=MAN_ON_TARGET; board[0][1]=END_OF_LINE;
+  board[1][0]=TARGET; board[1][1]=END_OF_LINE;
+  board[2][0]=FREE; board[2][1]=END_OF_LINE;
+  board_nbl=3;
+  man_l=0; man_c=0;
+  if(can_move_D()==false) return false;
+  move_D();
+  if(board[1][0] != TARGET) return false;
+  if(board[2][0] != MAN_ON_TARGET) return false;
+  if(man_l!=1) return false;
+  return true;
+}
+
+/* position : ...@
+ *            ...$
+ *            ...#
+ * can_move_D() return false 
+*/
+bool test_d009() {
+  board[0][0]=MAN_ON_FREE; board[0][1]=END_OF_LINE;
+  board[1][0]=CRATE_ON_FREE; board[1][1]=END_OF_LINE;
+  board[2][0]=WALL;board[2][1]=END_OF_LINE;
+  board_nbl=3;
+  man_l=0; man_c=0;
+  if(can_move_D()==false) return false;
+}
+
+/* position : ...a
+ *            ...$
+ *            ...#
+ * can_move_D() return false                              ...
+*/
+bool test_d010() {
+  board[0][0]=MAN_ON_TARGET; board[0][1]=END_OF_LINE;
+  board[1][0]=CRATE_ON_FREE; board[1][1]=END_OF_LINE;
+  board[2][0]=WALL;board[2][1]=END_OF_LINE;
+  board_nbl=3;
+  man_l=0; man_c=0;
+  if(can_move_D()==false) return false;
+}
+
+/* position : ...@
+ *            ...*
+ *            ...#
+ * can_move_D() return true 
+ * move_D() modifie la position : ...
+ *                                ...a
+ *                                ...#
+*/
+bool test_d011() {
+  board[0][0]=MAN_ON_FREE; board[0][1]=END_OF_LINE;
+  board[1][0]=TARGET; board[1][1]=END_OF_LINE;
+  board[2][0]=WALL;board[2][1]=END_OF_LINE;
+  board_nbl=3;
+  man_l=0; man_c=0;
+  if(can_move_D()==false) return false;
+  move_D();
+  if(board[0][0] != FREE) return false;
+  if(board[1][0] != MAN_ON_TARGET) return false;
+  if(board[2][0] != WALL) return false;
+  if(man_l!=1) return false;
+  return true;
+}
+
+/* position : ...a
+ *            ...*
+ *            ...#
+ * can_move_D() return true 
+ * move_D() modifie la position : ...*
+ *                                ...a
+ *                                ...#
+*/
+bool test_d012 () {
+  board[0][0]=MAN_ON_TARGET; board[0][1]=END_OF_LINE;
+  board[1][0]=TARGET; board[1][1]=END_OF_LINE;
+  board[2][0]=WALL;board[2][1]=END_OF_LINE;
+  board_nbl=3;
+  man_l=0; man_c=0;
+  if(can_move_D()==false) return false;
+  move_D();
+  if(board[0][0] != TARGET) return false;
+  if(board[1][0] != MAN_ON_TARGET) return false;
+  if(board[2][0] != WALL) return false;
+  if(man_l!=1) return false;
+  return true;
+}
 #endif
